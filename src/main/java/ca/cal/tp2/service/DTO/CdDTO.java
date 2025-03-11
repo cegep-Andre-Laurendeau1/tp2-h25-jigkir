9721711b0b05709cfa6e9719427f812a0f3c152a
leo_laptop <leotran1460740@gmail.com>
2025-03-10 20:57:33 -0400

    Ajouter la classe CdDTO pour représenter les informations des CD, incluant artiste, durée et genre

---
+++ src/main/java/ca/cal/tp2/service/DTO/CdDTO.java
@@ -0,0 +1,29 @@
+package ca.cal.tp2.service.DTO;
+
+import java.time.LocalDate;
+
+public class CdDTO extends DocumentDTO {
+    private String artiste;
+    private int duree;
+    private String genre;
+    private final int dureeEmpruntSem = 2;
+
+    public CdDTO (String titre, LocalDate anneePublication,  String artiste, int duree, String genre) {
+        super(titre, anneePublication);
+        this.artiste = artiste;
+        this.duree = duree;
+        this.genre = genre;
+    }
+
+    @Override
+    public String toString() {
+        return "CdDTO{" +
+                ", titre='" + getTitre() + '\'' +
+                ", anneePublication=" + getAnneePublication() +
+                "artiste='" + artiste + '\'' +
+                ", duree=" + duree +
+                ", genre='" + genre + '\'' +
+                ", dureeEmpruntSem=" + dureeEmpruntSem +
+                '}';
+    }
+}
