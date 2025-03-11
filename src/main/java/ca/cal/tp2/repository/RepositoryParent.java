5a7bce1e3d62eafa63a4f64f58294748af635028
leo_laptop <leotran1460740@gmail.com>
2025-03-10 20:57:23 -0400

    Ajouter la classe RepositoryParent pour gérer la connexion à la base de données et créer les tables CD, LIVRE et DVD

---
+++ src/main/java/ca/cal/tp2/repository/RepositoryParent.java
@@ -0,0 +1,60 @@
+package ca.cal.tp2.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class RepositoryParent {
+    static final String JDBC_DRIVER = "org.h2.Driver";
+    static final String DB_URL = "jdbc:h2:mem:tp2Leo;DB_CLOSE_DELAY=-1";
+    static final String USER = "sa";
+    static final String PASS = "";
+    static Connection conn;
+
+    static {
+        String documentSQL =
+                "(id INTEGER NOT NULL," +
+                " titre VARCHAR(255), " +
+                " anneepublication DATE, " +
+                " nombreexemplaire INTEGER, ";
+        // STEP 1: Register JDBC driver
+        try {
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            Statement stmt = conn.createStatement();
+            String sql = "CREATE TABLE  CD " +
+                    documentSQL +
+                    " artiste VARCHAR(255), " +
+                    " duree INTEGER, " +
+                    " genre VARCHAR(255),"+
+                    "PRIMARY KEY (id)"+
+                    ")";
+            stmt.executeUpdate(sql);
+
+            stmt = conn.createStatement();
+            sql =  "CREATE TABLE  LIVRE " +
+                    documentSQL +
+                    "ISBN VARCHAR(255), " +
+                    " auteur VARCHAR(255), " +
+                    " editeur VARCHAR(255), " +
+                    " nombrepages INTEGER, " +
+                    "PRIMARY KEY (id)"+
+                    ")";
+            stmt.executeUpdate(sql);
+
+            stmt = conn.createStatement();
+            sql =  "CREATE TABLE  DVD " +
+                    documentSQL +
+                    " directeur VARCHAR(255), " +
+                    " duree INTEGER, " +
+                    " genre VARCHAR(255), " +
+                    "PRIMARY KEY (id)"+
+                    ")";
+            stmt.executeUpdate(sql);
+            System.out.println("Création tables terminés");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
