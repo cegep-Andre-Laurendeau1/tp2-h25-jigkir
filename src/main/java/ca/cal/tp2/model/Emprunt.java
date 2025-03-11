4ef2a289594a6d9123fcea7ea8bb580db4e20383
leo_laptop <leotran1460740@gmail.com>
2025-03-10 20:55:58 -0400

    Ajouter la classe Emprunt pour représenter les emprunts avec les attributs nécessaires et les méthodes de conversion en DTO

---
+++ src/main/java/ca/cal/tp2/model/Emprunt.java
@@ -0,0 +1,49 @@
+package ca.cal.tp2.model;
+
+import ca.cal.tp2.service.DTO.EmpruntDTO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Emprunt {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate dateEmprunt;
+    private String status;
+    @ManyToOne
+    private  Emprunteur emprunteur;
+    @OneToMany
+    private List<EmpruntDetails> empruntDetails;
+
+    public Emprunt(LocalDate dateEmprunt, String status, List<EmpruntDetails> empruntDetails, Emprunteur emprunteur) {
+        this.dateEmprunt = dateEmprunt;
+        this.status = status;
+        this.empruntDetails = empruntDetails;
+        this.emprunteur = emprunteur;
+    }
+    public Emprunt(LocalDate dateEmprunt, String status, Emprunteur emprunteur) {
+        this.dateEmprunt = dateEmprunt;
+        this.status = status;
+        this.emprunteur = emprunteur;
+    }
+    public String toString() {
+        return "Emprunt{" +
+                "id=" + id +
+                ", dateEmprunt=" + dateEmprunt +
+                ", status='" + status + '\'' +
+                '}';
+    }
+
+    public EmpruntDTO toDTO() {
+        return new EmpruntDTO(this.dateEmprunt, this.status, this.emprunteur);
+    }
+}
