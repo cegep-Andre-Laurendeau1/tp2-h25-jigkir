918c614c95ad1c70e8f49efd2ed51465fd27a12e
leo_laptop <leotran1460740@gmail.com>
2025-03-10 20:55:36 -0400

    Ajouter la classe Amende pour représenter les amendes avec les attributs nécessaires et les annotations JPA

---
+++ src/main/java/ca/cal/tp2/model/Amende.java
@@ -0,0 +1,24 @@
+package ca.cal.tp2.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Amende {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double montant;
+    private LocalDate dateCreation;
+    private boolean status;
+
+    @ManyToOne
+    private Emprunteur emprunteur;
+}
