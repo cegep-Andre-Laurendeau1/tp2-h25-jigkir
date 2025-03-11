91c80f08cafcbe6306a26c5ebe7fc998d2a89d09
leo_laptop <leotran1460740@gmail.com>
2025-03-10 20:59:06 -0400

    Ajouter la classe LivreDTO pour représenter les informations des livres, incluant ISBN, auteur, éditeur et nombre de pages

---
+++ src/main/java/ca/cal/tp2/service/DTO/LivreDTO.java
@@ -0,0 +1,31 @@
+package ca.cal.tp2.service.DTO;
+
+import java.time.LocalDate;
+
+public class LivreDTO extends DocumentDTO{
+    private String ISBN;
+    private String auteur;
+    private String editeur;
+    private int nombrePages;
+    private final int dureeEmpruntSem = 3;
+    public LivreDTO (String titre, LocalDate anneePublication, String ISBN, String auteur, String editeur, int nombrePages) {
+        super(titre, anneePublication);
+        this.ISBN = ISBN;
+        this.auteur = auteur;
+        this.editeur = editeur;
+        this.nombrePages = nombrePages;
+    }
+
+    @Override
+    public String toString() {
+        return "LivreDTO{" +
+                ", titre='" + getTitre() + '\'' +
+                ", anneePublication=" + getAnneePublication() +
+                "ISBN='" + ISBN + '\'' +
+                ", auteur='" + auteur + '\'' +
+                ", editeur='" + editeur + '\'' +
+                ", nombrePages=" + nombrePages +
+                ", dureeEmpruntSem=" + dureeEmpruntSem +
+                '}';
+    }
+}
