76755084a948c747c88a22937122897a67a13b72
leo_laptop <leotran1460740@gmail.com>
2025-03-10 20:50:43 -0400

    Refactoriser la classe principale pour utiliser les services JPA et améliorer la fonctionnalité de gestion des documents

--- src/main/java/ca/cal/tp2/Main.java
+++ src/main/java/ca/cal/tp2/Main.java
@@ -1,93 +1,130 @@
 package ca.cal.tp2;
 
-import ca.cal.tp2.service.dto.EmpruntDTO;
-import ca.cal.tp2.service.dto.EmprunteurDTO;
-import ca.cal.tp2.service.dto.LivreDTO;
-import ca.cal.tp2.model.Amende;
-import ca.cal.tp2.model.Emprunt;
-import ca.cal.tp2.model.Emprunteur;
-import ca.cal.tp2.model.Livre;
-import ca.cal.tp2.service.AmendeService;
-import ca.cal.tp2.service.DocumentService;
-import ca.cal.tp2.service.EmpruntService;
-import ca.cal.tp2.service.PreposeService;
+import ca.cal.tp2.repository.*;
+import ca.cal.tp2.service.EmprunteurService;
+import ca.cal.tp2.utils.TcpServer;
 
+import java.time.LocalDate;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, InterruptedException {
+    public static void main(String[] args) throws InterruptedException, SQLException {
+        // Votre script qui utilise votre API ici
         TcpServer.startTcpServer();
 
-        DocumentService documentService = new DocumentService();
-        EmpruntService empruntService = new EmpruntService();
-        AmendeService amendeService = new AmendeService();
-        PreposeService preposeService = new PreposeService();
+        //EmprunteurService emprunteurServiceJDBC = new EmprunteurService(new CdRepositoryJDBC(), new DvdRepositoryJDBC(), new LivreRepositoryJDBC());
+        EmprunteurService emprunteurServiceJPA = new EmprunteurService(new EmprunteurRepositoryJPA(), new DocumentRepositoryJPA(), new EmpruntDetailsRepositoryJPA(), new EmpruntRepositoryJPA());
 
-        // Create a LivreDTO
-        LivreDTO livreDTO = documentService.createInitialBook();
 
-        // Convert DTO to model and add document
-        Livre livre = convertToLivre(livreDTO);
-        documentService.addDocument(livre);
 
-        // Create an EmprunteurDTO
-        EmprunteurDTO emprunteurDTO = new EmprunteurDTO( "Alice", "alice@example.com", "1234567890" );
+//        emprunteurServiceJDBC.saveLivre("titre JDBC", LocalDate.of(2021, 1, 1),1,"ISBN","auteur","editeur",1);
+//        System.out.println(emprunteurServiceJDBC.getLivre(1L));
+//
+//        emprunteurServiceJDBC.saveCd("titre JDBC",LocalDate.of(2022, 1, 1),1,"artiste",1, "genre");
+//        System.out.println(emprunteurServiceJDBC.getCd(1L));
+//
+//        emprunteurServiceJDBC.saveDvd("titre JDBC",LocalDate.of(2023, 1, 1),1,"directeur", 1, "genre");
+//        System.out.println(emprunteurServiceJDBC.getDvd(1L));
 
-        // Register new borrower using DTO
-        Emprunteur emprunteur = preposeService.inscrireNouvelEmprunteur(
-            emprunteurDTO.getNom(),
-            emprunteurDTO.getEmail(),
-            emprunteurDTO.getTelephone()
-        );
+        System.out.println("#####################");
+        System.out.println("#Section getDocument#");
+        System.out.println("#####################");
 
-        // Create loan
-        if (emprunteur != null && empruntService.createEmprunt(emprunteur, livre)) {
-            System.out.println("Document emprunté avec succès!");
-        } else {
-            System.out.println("Impossible d'emprunter le document.");
+        emprunteurServiceJPA.saveLivre("titre JPA", LocalDate.of(2021, 1, 1),1,"ISBN","auteur","editeur",1);
+        System.out.println("\n");
+
+        System.out.println(emprunteurServiceJPA.getDocument(1L));
+        System.out.println("\n");
+
+        emprunteurServiceJPA.saveCd("titre JPA",LocalDate.of(2021, 1, 1),1,"artiste",1, "genre");
+        System.out.println("\n");
+        System.out.println(emprunteurServiceJPA.getDocument(2L));
+        System.out.println("\n");
+
+        emprunteurServiceJPA.saveDvd("titre JPA",LocalDate.of(2021, 1, 1),1,"directeur", 1, "genre");
+        System.out.println("\n");
+        System.out.println(emprunteurServiceJPA.getDocument(3L));
+        System.out.println("\n");
+
+        emprunteurServiceJPA.saveDvd("TITLE JPA",LocalDate.of(2021, 1, 1),1,"directeur", 1, "genre");
+        System.out.println("\n");
+        System.out.println(emprunteurServiceJPA.getDocument(4L));
+        System.out.println("\n");
+
+        emprunteurServiceJPA.saveDvd("title JPA",LocalDate.of(2021, 1, 1),1,"directeur", 1, "genre");
+        System.out.println("\n");
+        System.out.println(emprunteurServiceJPA.getDocument(5L));
+        System.out.println("\n");
+
+
+        System.out.println("###########################");
+        System.out.println("#Section ajouterEmprunteur#");
+        System.out.println("###########################");
+        emprunteurServiceJPA.ajouterEmprunteur("nom", "email", "numTelephone");
+        System.out.println("\n");
+
+        System.out.println("########################");
+        System.out.println("#Section saveExemplaire#");
+        System.out.println("########################");
+        emprunteurServiceJPA.saveExemplaire(30, 1L);
+        System.out.println("\n");
+
+        emprunteurServiceJPA.saveExemplaire(30, 2L);
+        System.out.println("\n");
+
+        System.out.println("###########################");
+        System.out.println("#Section rechercheDocument#");
+        System.out.println("###########################");
+        emprunteurServiceJPA.rechercheDocument("titre", LocalDate.of(2021, 1, 1));
+        System.out.println("\n");
+        emprunteurServiceJPA.rechercheDocument("titre");
+        System.out.println("\n");
+
+        emprunteurServiceJPA.rechercheDocument("JPA");
+        System.out.println("\n");
+
+        emprunteurServiceJPA.rechercheDocument(LocalDate.of(2021, 1, 1));
+        System.out.println("\n");
+
+        System.out.println("###########################");
+        System.out.println("#Section emrpunterDocument#");
+        System.out.println("###########################");
+        List<Long> list = new ArrayList<>();
+        list.add(1L);
+        list.add(2L);
+        emprunteurServiceJPA.emprunterDocument(list, 1L);
+        System.out.println("\n");
+
+        list.add(3L);
+        emprunteurServiceJPA.emprunterDocument(list, 1L);
+        System.out.println("\n");
+        try {
+            emprunteurServiceJPA.emprunterDocument(list, 1L);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
         }
+        System.out.println("\n");
 
-        // Get loans and convert to DTOs
-        List<Emprunt> emprunts = empruntService.getEmpruntsForEmprunteur(emprunteur);
-        List<EmpruntDTO> empruntsDTO = emprunts.stream()
-            .map(Main::convertToEmpruntDTO)
-            .collect(Collectors.toList());
-        System.out.println("Historique des emprunts: " + empruntsDTO.size());
+        System.out.println("################################");
+        System.out.println("#Section getDocumentsEmprunteur#");
+        System.out.println("################################");
+        emprunteurServiceJPA.getDocumentsEmprunteur(1L);
+        emprunteurServiceJPA.getDocumentsEmprunteur(400000L);
 
-        // Return document
-        if (empruntService.retournerDocument(emprunteur, livre)) {
-            System.out.println("Document retourné avec succès!");
-        } else {
-            System.out.println("Échec du retour du document.");
-        }
+        System.out.println("\n");
 
-        // Get fines
-        List<Amende> amendes = amendeService.getAllAmendesByEmprunteur(emprunteur);
-        System.out.println("Amendes impayées: " + amendes.size());
+        System.out.println("########################");
+        System.out.println("#Section getDocumentFin#");
+        System.out.println("########################");
+        System.out.println(emprunteurServiceJPA.getDocument(1L));
+        System.out.println("\n");
+        System.out.println(emprunteurServiceJPA.getDocument(2L));
+        System.out.println("\n");
+        System.out.println(emprunteurServiceJPA.getDocument(3L));
+        System.out.println("\n");
 
         Thread.currentThread().join();
-    }
-
-    private static Livre convertToLivre(LivreDTO dto) {
-        return new Livre(
-            dto.getDocumentId(),
-            dto.getTitre(),
-            dto.getNbExemplaires(),
-            dto.getIsbn(),
-            dto.getAuteur(),
-            dto.getEditeur(),
-            dto.getNbPages()
-        );
-    }
-
-    private static EmpruntDTO convertToEmpruntDTO(Emprunt emprunt) {
-        return new EmpruntDTO(
-            emprunt.getBorrowID(),
-            emprunt.getEmprunteur().getId(),
-            emprunt.getStatus(),
-            emprunt.getDateEmprunt()
-        );
     }
 }
