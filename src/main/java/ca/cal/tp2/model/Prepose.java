7e868c700ecb5fd3e1b78ec34399129bf0afc295
leo_laptop <leotran1460740@gmail.com>
2025-03-10 20:56:19 -0400

    Ajouter la classe Prepose pour représenter les préposés avec des méthodes pour générer des rapports d'amendes et d'emprunts

---
+++ src/main/java/ca/cal/tp2/model/Prepose.java
@@ -0,0 +1,16 @@
+package ca.cal.tp2.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Prepose extends Utilisateur {
+    public List<Amende> rapportAmendes(){
+        List<Amende> amendes = new ArrayList<Amende>();
+        return amendes;
+    }
+    public List<Emprunt> rapportEmprunts(){
+        List<Emprunt> emprunt = new ArrayList<>();
+        return emprunt;
+    }
+
+}
